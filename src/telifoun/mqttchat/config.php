b1826602cd0dbc4a24dbbdaebfabea0ac23d1b56
mohamed <m.gaddour@yahoo.fr>
2020-02-17 21:31:34 +0100

    first commit

--- src/telifoun/mqttchat/config.php
+++ src/telifoun/mqttchat/config.php
@@ -11,23 +11,28 @@ namespace telifoun\mqttchat;
  
  class config{
      
-    /** telifoun mqttchat Client_id **/
-    const CLIENT_ID="FX5501";
-    /** telifoun mqttchat Client_secret **/
-    const CLIENT_SECRET="FX5502";
-         
+    /** telifoun mqttchat app_id **/
+    const APP_ID="mqttchat-87226030";
+    /** telifoun mqttchat app_secret **/
+    const APP_SECRET="mqttchat-56vvfbvnpe0uvuid";  
+    
+    
     /** telifoun mqttchat rest server **/
-    const MQTTCHAT_REST_SERVER="http://cluster1.telifoun.com/rest" ;
+    const MQTTCHAT_REST_SERVER="http://cluster1.telifoun.com/rest" ;    
      
     /** telifoun mqttchat oauth2 server url **/
     const OAUTH2_TOKEN_URL="/oauth2/token";
     
+     /** telifoun mqttchat users url **/
     const REST_USERS_URL ="/v1.0/users";
     
+     /** telifoun mqttchat ofriends url **/
     const REST_FRIENDS_URL="/v1.0/friends";    
     
+     /** telifoun mqttchat settings url **/
     const REST_SETTINGS_URL="/v1.0/settings";
     
+     /** telifoun mqttchat actions url **/
     const REST_ACTIONS_URL="/v1.0/actions";
     
      
--- src/telifoun/mqttchat/oauth2.php
+++ src/telifoun/mqttchat/oauth2.php
@@ -9,34 +9,19 @@ namespace telifoun\mqttchat;
 
 class oauth2 {
     
-    private $_provider;   
    
     /**
-     * 
+     *
+     * @var type 
      */
-    public function __construct(){ 
-    $this->_provider = new \League\OAuth2\Client\Provider\GenericProvider([
-    'clientId'                => config::CLIENT_ID,    // The client ID assigned to you by the provider
-    'clientSecret'            => config::CLIENT_SECRET,   // The client password assigned to you by the provider
-    'urlAccessToken'          =>config::OAUTH2_TOKEN_URL,
-    'redirectUri'             => '',
-    'urlAuthorize'            => '',
-    'urlResourceOwnerDetails' => ''
-    ]);    
-    }
-    
-    
+    private $_rest_client;    
     /**
      * 
+     * @param type $app_secret
      */
-    private function _getAccessToken(){
-      try {          
-      $accessToken = $this->_provider ->getAccessToken('client_credentials');  
-      return $accessToken->getToken() ;
-      } catch (\League\OAuth2\Client\Provider\Exception\IdentityProviderException $ex) {
-      throw new myException ("OAUTH2 server error : " .$ex->getMessage(),0);
-      }  
-    }
+    public function __construct(){       
+      $this->_rest_client = new \GuzzleHttp\Client();
+    }       
     
     /**
      * 
@@ -46,16 +31,12 @@ class oauth2 {
      * @return type
      */
     public function AuthenticatedRequest($method,$url,$data=array()){  
-     try {   
-        $options['body'] = json_encode($data);
-        $options['headers']['content-type'] = 'application/json';
-        $request= $this->_provider->getAuthenticatedRequest(
-            $method,
-            $url,
-            $this->_getAccessToken(),
-            $options
-        );
-        return $this->_provider->getParsedResponse($request);     
+     try {  
+       $res= $this->_rest_client->request($method,$url,[
+           'auth' => [config::APP_ID, config::APP_SECRET],
+           'body' => json_encode($data)
+       ]);        
+       return $res->getBody()->getContents();  
      }catch(\Exception $ex){
       throw new myException ($ex->getMessage(),0);
      }
