6f696feec7537901cf81614ca92b3aab73286cee
Administrateur <m.gaddour@yahoo.fr>
2021-07-06 10:33:04 +0100

    adding notifications

--- src/telifoun/mqttchat/config.php
+++ src/telifoun/mqttchat/config.php
@@ -10,14 +10,14 @@ namespace telifoun\mqttchat;
  class config{
      
     /** telifoun mqttchat app_id **/
-    const APP_ID="mqttchat-62949509";
+    const APP_ID="";
     /** telifoun mqttchat app_secret **/
-    const APP_SECRET="mqttchat-cyvepgwhc2xoud30";  
+    const APP_SECRET="";  
     
     
     /** telifoun mqttchat rest server **/
-    //const MQTTCHAT_REST_SERVER="https://cluster1.telifoun.com/api/v1.0" ;   
-   const MQTTCHAT_REST_SERVER="http://10.0.1.3/api/v1.0" ;   
+    const MQTTCHAT_REST_SERVER="https://cluster1.telifoun.com/api/v1.0" ;   
+
      /** telifoun mqttchat users url **/
     const REST_USERS_URL ="/users";
     
